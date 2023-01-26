87223a6bbdc2d9aa914f1a01ba6417ce7e7b0080
IreneQuero <queroirene@gmail.com>
2023-01-26 16:40:02 +0000

    test correcto

--- src/app.py
+++ src/app.py
@@ -26,17 +26,47 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def get_all_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members
+    if members == None:
+        return jsonify({"message": "Ha ocurrido un error"}), 400
 
     return jsonify(response_body), 200
+
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member_by_id(member_id):
+    response_body = jackson_family.get_member(member_id)
+    if response_body == None:
+        return jsonify({"message": "Ha ocurrido un error"}), 400
+
+    return jsonify(response_body), 200
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    response_body = {"done":jackson_family.delete_member(member_id)} 
+    if response_body == None:
+        return jsonify({"message": "Ha ocurrido un error"}), 400
+
+    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.json
+    
+    if request_body == None:
+        return jsonify({"message": "Ha ocurrido un error"}), 400
+
+    jackson_family.add_member(request_body)
+    response_body = ""
+    
+    return jsonify(response_body), 200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
